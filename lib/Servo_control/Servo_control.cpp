e6aec2057075f9a227ed4277c5406133b10be3f0
HenryCROSS <henry583050973@gmail.com>
2024-01-20 12:48:24 -0500

    change servo degree

--- lib/Servo_control/Servo_control.cpp
+++ lib/Servo_control/Servo_control.cpp
@@ -4,30 +4,37 @@
 // third party
 
 // ours
-#include <config.h>
 #include <Ultrasonic.h>
+#include <config.h>
 
-void Servo_Start(){
+int degree_lz[] = {15, 90, 165};
+
+void Servo_Start()
+{
     servo1.write(servo1_init);
 }
 
-void Scan_for_Obj(){
-  for(int i=15;i<=165;i++){  
-    servo1.write(i);
-    delay(30);
-    distance = detect_obj_distance(TRIGGER_PIN, ECHO_PIN);
-    Serial.print(i);
-    Serial.print(",");
-    Serial.print(distance);
-    Serial.print(".");
-  }
-  for(int i=165;i>15;i--){  
-    servo1.write(i);
-    delay(30);
-    distance = detect_obj_distance(TRIGGER_PIN, ECHO_PIN);
-    Serial.print(i);
-    Serial.print(",");
-    Serial.print(distance);
-    Serial.print(".");
-  }
+// 15 90 165
+void Scan_for_Obj()
+{
+    for (int idx = 0; idx < sizeof(degree_lz); idx++)
+    {
+        servo1.write(degree_lz[idx]);
+        delay(30);
+        distance = detect_obj_distance(TRIGGER_PIN, ECHO_PIN);
+        Serial.print(degree_lz[idx]);
+        Serial.print(",");
+        Serial.print(distance);
+        Serial.print(".");
+    }
+    for (int idx = sizeof(degree_lz) - 1; idx >= 0; idx--)
+    {
+        servo1.write(degree_lz[idx]);
+        delay(30);
+        distance = detect_obj_distance(TRIGGER_PIN, ECHO_PIN);
+        Serial.print(degree_lz[idx]);
+        Serial.print(",");
+        Serial.print(distance);
+        Serial.print(".");
+    }
 }
