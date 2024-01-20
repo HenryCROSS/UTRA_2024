ae6470cb96875d5161acbe63878098ba4c8efcea
Polynomial <ecnuzjy@qq.com>
2024-01-19 20:03:48 -0500

    加入了第三方库

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
-
+#include <Servo.h>
+#include <Adafruit_BNO055.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <utility/imumaths.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 // put function declarations here:
 int myFunction(int, int);
 
